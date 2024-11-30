31043c198296968d7bc3cbba403548c6ff02de73
Jaehyeong Lee <146356593+Lata17@users.noreply.github.com>
2024-11-30 14:29:25 +0900

    refactor: Material Calendarview 라이브러리 추가

--- MomentUp/app/build.gradle.kts
+++ MomentUp/app/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+    implementation (libs.material.calendarview)
+    implementation (libs.threetenabp)
 }
